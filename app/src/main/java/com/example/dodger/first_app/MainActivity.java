bf70d79e09b88dd8c59f58c0c1c42e1c228271b5
Navigator365 <40570437+Navigator365@users.noreply.github.com>
2018-11-17 08:01:17 -0500

    Change name and add new file page

--- app/src/main/java/com/example/dodger/first_app/MainActivity.java
+++ app/src/main/java/com/example/dodger/first_app/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     int oldValue;
-    public static final String EXTRA_MESSAGE = "com.example.Name_Machine.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.example.Vending_Machine.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendTheMessage(View v) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, VendingMachinePickerActivity.class);
         EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
---
+++ app/src/main/java/com/example/dodger/first_app/OutputActivity.java
@@ -0,0 +1,4 @@
+package com.example.dodger.first_app;
+
+public class OutputActivity {
+}
