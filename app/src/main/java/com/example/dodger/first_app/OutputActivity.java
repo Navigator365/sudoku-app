8f233c7e949c06af0bc2166341a9a3f6c8c214bd
Navigator365 <40570437+Navigator365@users.noreply.github.com>
2018-11-21 18:44:49 -0500

    Set up onClickListeners

--- app/src/main/java/com/example/dodger/first_app/OutputActivity.java
+++ app/src/main/java/com/example/dodger/first_app/OutputActivity.java
@@ -1,7 +1,8 @@
 package com.example.dodger.first_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class OutputActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class OutputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
+        Intent intent = getIntent();
     }
 }
--- app/src/main/java/com/example/dodger/first_app/VendingMachinePickerActivity.java
+++ app/src/main/java/com/example/dodger/first_app/VendingMachinePickerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class VendingMachinePickerActivity extends AppCompatActivity {
@@ -20,7 +21,34 @@ public class VendingMachinePickerActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
-    public void computePurchase(View v){
+
+    public void computePurchase(View v) {
         Intent intent = new Intent(this, OutputActivity.class);
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        double comparisonValue = Double.parseDouble(message);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+
+        setClickListener(button3);
+        setClickListener(button4);
+        setClickListener(button5);
+        setClickListener(button6);
+        setClickListener(button7);
+        setClickListener(button8);
+        
+    }
+
+    private void setClickListener(Button listenerObject){
+        listenerObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
