bee0accbed0711641d811069862d06aaa27210aa
Navigator365 <40570437+Navigator365@users.noreply.github.com>
2018-11-23 17:53:09 -0500

    Semi-broke it again

--- app/src/main/java/com/example/dodger/first_app/OutputActivity.java
+++ app/src/main/java/com/example/dodger/first_app/OutputActivity.java
@@ -12,9 +12,54 @@ public class OutputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_output);
         Intent intent = getIntent();
         String stringComparisonValue = intent.getStringExtra(VendingMachinePickerActivity.WHICH_BUTTON_PRESSED);
-        //String inputMoney = getIntent().getExtras().getString(MainActivity.EXTRA_MESSAGE);
 
+        String inputMoney = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.textView3);
-        textView.setText(stringComparisonValue);
+        double fillerValue = 0.0;
+
+        if(Double.valueOf(stringComparisonValue) == 3.0) {
+            if (Double.valueOf(inputMoney) - 1.99 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 1.99;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        }
+        if(Double.valueOf(stringComparisonValue) == 4.0) {
+            if (Double.valueOf(inputMoney) - 0.50 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 0.50;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        }  if(Double.valueOf(stringComparisonValue) == 5.0) {
+            if (Double.valueOf(inputMoney) - 3.50 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 3.50;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        }  if(Double.valueOf(stringComparisonValue) == 6.0) {
+            if (Double.valueOf(inputMoney) - 5.47 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 5.47;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        }  if(Double.valueOf(stringComparisonValue) == 7.0) {
+            if (Double.valueOf(inputMoney) - 2.99 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 2.99;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        }  if(Double.valueOf(stringComparisonValue) == 8.0) {
+            if (Double.valueOf(inputMoney) - 6.54 < 0.0) {
+                textView.setText(R.string.no_money_string);
+            } else {
+                fillerValue = Double.valueOf(inputMoney) - 6.54;
+                textView.setText(R.string.end_string + String.valueOf(fillerValue));
+            }
+        } textView.setText(R.string.no_money_string);
     }
 }
